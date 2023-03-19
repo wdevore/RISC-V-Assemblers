9e4ed98fafc550e985b1c92e6b3f3bf827eb2c12
wdevore <william.quartz@gmail.com>
2023-03-19 12:01:25 -0400

    initial port from Ranger RISC-V project

---
+++ basic/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+
+	"github.com/atotto/clipboard"
+	"github.com/wdevore/RISC-V-Assemblers/assemblers"
+)
+
+func main() {
+	processLine()
+	// testBaudGenerator()
+}
+
+func processLine() {
+	assemblyPro := os.Args[1:]
+
+	var fileName = "assembly.json"
+
+	if len(assemblyPro) > 0 {
+		fileName = assemblyPro[0]
+	}
+
+	// Open our jsonFile
+	jsonFile, err := os.Open(fileName)
+
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully Opened assembly.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var context map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &context)
+
+	fmt.Println("Assembling: ", context["Assembly"])
+
+	rxpr, _ := regexp.Compile(`([a-z]+)`)
+
+	ass := fmt.Sprintf("%s", context["Assembly"])
+	fields := rxpr.FindStringSubmatch(ass)
+
+	instruction := fields[1]
+
+	machineCode, err := assemblers.Dispatch(instruction, context)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Machine Code: ", machineCode)
+	err = clipboard.WriteAll(machineCode)
+	if err != nil {
+		panic(err)
+	}
+}
